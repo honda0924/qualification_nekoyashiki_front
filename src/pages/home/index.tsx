dd8567ad3328fd3401c2a274d738bc446c337154
nekoyashiki <47511135+honda0924@users.noreply.github.com>
2021-04-20 07:15:44 +0900

    initial commit

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Header } from "../components/header";
+import { ExamPanel } from "../components/examPanel";
+export const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <ExamPanel/>
+    </div>
+  );
+};
